b417e144023858643fe8317e29a4315d0c95a98e
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-24 23:08:39 -0600

    Create sexto.cpp

---
+++ sexto.cpp
@@ -0,0 +1,25 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+*/
+
+int  principal ( int  argc ,  caracteres  const  * argv []){
+	
+	int  un , b , c ;
+	scanf  ( "% d" , & a );
+
+	for( int  i  =  0 ;  i  <=  un ;  + + i ){
+		b = 1 ;
+		while  ( b <= i ){
+			printf ( "" );
+			+ + b ;	
+		}
+		c = i ;
+		while( C < una ) {
+			+ + C ;
+			printf ( "*" );
+		}
+		printf ( " \ n " );
+	}
+	return  0 ;
+}
