7795a95e90a7d87b4b43155e89463f51084f0b06
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-24 23:09:12 -0600

    Create septimo.cpp

---
+++ septimo.cpp
@@ -0,0 +1,23 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+*/
+
+# include <stdio.h>
+int  principal ( int  argc ,  caracteres  const  * argv []){
+	int  a , b, un;
+	scanf  ( "% d" , &a );
+
+	for(int i=1;i<=un;i++ )
+	{
+		b = 1 ;
+		while( b <= i )
+		{
+			printf ( "*" );
+			+ + b ;	
+		}
+		printf ( " \ n " );
+		
+	}
+	return 0 ;
+}
