86c8705fe0d34d21962dc7740fda9421b74b9c83
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-24 23:05:30 -0600

    Create Segundo.cpp

---
+++ Segundo.cpp
@@ -0,0 +1,12 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+*/
+
+# include <stdio.h>
+int  main(){
+	int  una,b ;
+	scanf( "%d" , &b );
+	printf( "Valor:% d" , b );
+	return  0 ;	
+}
