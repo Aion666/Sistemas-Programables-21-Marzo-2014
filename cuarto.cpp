92110528a1abfa502039f84e8963417a979d450c
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-24 23:06:28 -0600

    Create cuarto.cpp

---
+++ cuarto.cpp
@@ -0,0 +1,21 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+*/
+
+# include <stdio.h>
+int  principal ( int  argc , caracteres  const  * argv []){
+	int  a ,b,un ;
+	scanf  ( "% d" , &a );
+
+	for(int i=un;i>=1;i-- ){
+		b = 1 ;
+		while(b<=i){
+			printf ( "*" );
+			b++;	
+		}
+		printf ( " \ n " );
+		
+	}
+	return 0 ;
+}
