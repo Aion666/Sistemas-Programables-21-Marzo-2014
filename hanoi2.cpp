3ddce71a1729cbd7c416b5e2d04f613e9d554ebd
Miguel Santos Gonzalez <theblackhelena_232@hotmail.com>
2014-03-25 19:30:27 -0600

    Create hanoi2.cpp

---
+++ hanoi2.cpp
@@ -0,0 +1,21 @@
+/*	Sistemas programables
+	Alumno: Santos Gonzalez Miguel Angel
+	7-8 am
+	Hanoi con numero de movimientos
+*/
+
+#include<stdio.h>
+int hanoi(int n){
+    if(n==1)
+        return 1;
+    else
+        return 2 * hanoi(n-1) + 1;
+}
+int main(){
+    int disc, mov;
+    printf("Torres de Hanoi \n");
+    printf("Numero de discos: ");
+	scanf("%i",&disc);
+    printf("\tNumero de Movimientos : %i\n", hanoi(disc));
+    return 0;
+}
